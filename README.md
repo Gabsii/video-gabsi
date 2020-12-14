739fcd0b6677762a8674781d2af3de516ec0ebe6
Lukas Gabsi <lukas.gabsi@towa.at>
2020-12-14 12:58:12 +0100

    bot can now play sound in voice channels

---
+++ README.md
@@ -0,0 +1,7 @@
+## Resources
+
+- [Discord Bot](https://www.youtube.com/watch?v=BmKXBVdEV0g)
+- [Chrome Extension](https://www.youtube.com/watch?v=1VOFxZ5nBMA)
+
+## TODO
+- automatic language detection for TTS --> [https://www.npmjs.com/package/languagedetect]
--- bot/index.js
+++ bot/index.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 
 const { Client } = require('discord.js');
+const gtts = require('gtts');
 
 const client = new Client();
 client.on('ready', () => {
@@ -10,11 +11,27 @@ client.on('ready', () => {
 });
 
 client.on('message', async (message) => {
-  if(!message.author.bot) {
-    const voiceChannel = client.channels.cache.find((channel) => channel.name === process.env.VOICE_CHANNEL);
-    const connection = await voiceChannel.join();
-    // console.log(connection);
-    message.reply('hello');
+  if(message.author.username === 'Video Gabsi') {
+    if(message.embeds.length > 0){
+      const audio = new gtts(`Gabsi schaut: ${message.embeds[0].title}`, 'en');
+      audio.save('./bot/sound/message.mp3', function (err, result) {
+        if(err) { throw new Error(err) }
+        console.log('Success! Open file message.mp3 to hear result.');
+      });
+      
+      const voiceChannel = client.channels.cache.find((channel) => channel.name === process.env.VOICE_CHANNEL);
+      const connection = await voiceChannel.join();
+      const dispatcher = connection.play('./bot/sound/message.mp3');
+      
+      // todo: use stream instead of file
+      // const dispatcher = connection.play(audio.stream().pipe());
+      
+      dispatcher.on('finish', () => {
+        console.log('Finished playing!');
+        dispatcher.destroy(); // end the stream
+        voiceChannel.leave();
+      });
+    }
   }
 })
 
